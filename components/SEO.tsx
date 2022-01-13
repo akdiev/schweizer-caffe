9b80932b041bfe607d4c5bc67bc8bfc940eecc63
Armin Aki Hodzic <armin.h29@gmail.com>
2022-01-13 16:09:14 +0100

    SEO

---
+++ components/SEO.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Head from "next/head";
+
+export default function SEO() {
+  return (
+    <Head>
+      {/* Global site tag (gtag.js) - Google Analytics {" "}
+      <script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-Y08F83Q7BQ"
+      ></script>{" "}
+      <script>
+        {" "}
+        {`          window.dataLayer = window.dataLayer || [];          function gtag(){dataLayer.push(arguments);}          gtag('js', new Date());          gtag('config', 'G-Y08F83Q7BQ');        `}{" "}
+      </script> */}
+    </Head>
+  );
+}
